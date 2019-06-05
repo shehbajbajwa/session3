d8e1ee7fecd397a3161d79ba475bf6405e07210b
shehbaj bajwa <bajwashehbaj@gmail.com>
2019-06-05 19:58:46 +0530

    session 3

---
+++ session3G.py
@@ -0,0 +1,41 @@
+#loop or Iteration
+number = 5
+i = 1
+""" print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+i += 1
+print(number, i, " s are ", (number * i))
+"""
+
+ #while #for there are 2 loops in python
+#loop is used to process alot of data
+number = int(input("enter a number:"))
+i = 1
+"""while<=10:
+    print(number, i, " s are ", (number * i))
+    i += 1
+#there is nothing like ++ in python
+"""
+"""
+for i in range(1,11):
+    print(number, i, " s are ", (number * i))
+"""
+
+
+for i in range(1,20,2):   #use this for to display odd and even numbers/answers
+    print(number, i, " s are ", (number * i))
